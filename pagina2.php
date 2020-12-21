084254fdd97279d00ae5aa9f0e51f3509d145510
hector <hferb10@gmail.com>
2020-12-21 13:15:00 +0100

    Create pagina2.php

---
+++ pagina2.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "<h1> TÍTULO </h1><br/>"; 
+
+echo "<p> Texto dela página .... </p>";
+
+echo "<p> Texto dela página .... </p>";
+
+echo "<p> Texto dela página .... </p>";
+
+echo "<p> Texto dela página .... </p>
+         <h1> Holaaaaaa .... </h1>
+     <h2> Texto dela página .... </h2>
+     ";
+
+?>
