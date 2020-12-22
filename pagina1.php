81b8a54a5b35be42c2202c0a4042756a35324302
hector <hferb10@gmail.com>
2020-12-22 12:21:35 +0100

    Update pagina1.php

--- pagina1.php
+++ pagina1.php
@@ -9,93 +9,34 @@
 
 echo "<h1> DORAEMON </h1><br/>"; 
 
-echo "<p> doraemon es un tio muy way </p>";
-
-echo "<p> Texto dela página .... </p>";
-
-echo "<p> Texto dela página .... </p>";
-
-echo "<head>
-        <title>Este texto es el título del documento</title>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="estilos.css" type="text/css">
-<link href="css/estilos.css" rel="stylesheet" type="text/css" >
-        <style>
- 
-                .dv { background-image: url("rombos.jpg"); }
-        </style>
-</head>
-<body style="background-color:#EC7063;">
-        <header>
-                <div style="background-image: url('rombos.jpg'); border: 3px solid #186A3B ; "> <h1> GASTRONOMÍA NAVIDEÑA </h1>  </div>
-                <div class="dv"> Felices Fiestas</div>
-
-        </header>
-
-
-
-<img src="navidad1.jpg" width="700" height="700" align="right"/>
-
-
-        <main>
-                <a href="pagina1.php"> RECETAS </a><br/>
+echo "           <p class="mitexto1" align="left"> CONOCE A TU AMIGO EL GATO CÓSMICO</p>
 </br>
 </br>
-                <a href="pagina2.php"> POSTRES </a>
-
-        </main>
-        <section>
-</br>
-               </br>
-                        <p class="mitexto1" align="left"> “La navidad no es una fecha; es un estado en la mente” (Mary Ellen Chase) </p>
-</br>
-</br>
-                        <p class="mitexto2"> “No existe la Navidad ideal, solo la Navidad que usted decida crear como reflejo de sus valores, deseos, queridos y tradiciones” (Bill McKibben)</p>
-                       </br>
+                       
   </br>
  
                         <center>
 
-                        <h1> PIRULETAS DE NAVIDAD </h1>
-                        <p> INGREDIENTES</p>
-                        <span> Surtido variado de sprinkles con motivos o colores navideños </span>
+                        <h1> DORAEMON </h1>
+                        <p> Doraemon es un personaje ficticio protagonista de la serie de manga y anime del mismo nombre, creado por Fujiko Fujio, seudónimo de Hiroshi Fujimoto y Motoo Abiko. Doraemon es un gato robot que viaja en el tiempo desde el siglo XXI para ayudar a un niño preadolescente llamado Nobita Nobi. </p>
 </br>
-                        <span> 15 palitos para piruletas </span>
-</br>
-                        <span> 425 g de azúcar </span>
-</br>
-<span> 160 ml de azúcar invertido o sirope de maíz</span>
-</br>
-<span> 125 ml de agua</span>
-</br>
-<span> Una pizca de sal</span>
-</br>
-<span> Una o dos gotitas de aroma concentrado de Amaretto </span>
-</br>
-<p> Modo de preparación</p>
-<span> Ponemos los sprinkles y el acebo de fondant en varios cuencos pequeños o ramequines de tal forma que estén separados por colores y formas. </span>
-</br>
-<span> En un vaso ponemos los palitos, a un lado de los cuencos con sprinkles. Necesitaremos trabajar con ellos rápidamente antes de que se endurezcan las piruletas.</span>
-                       
-  </center>
-                        <br/><br/>
-                        <center>
-                       
-                       
-                       
-                       
-                       
-                        </center>
-                       
-        </section>
-                <br/>
-               
-<div style="background-image: url('rombos.jpg'); border: 3px solid #186A3B ; "> <h1> ¡A comer!</h1>  </div>
-                       
 
-</body>;
+<p> CURIOSIDADES</p>
+<span> Doraemon fue concebido originalmente por Hiroshi Fujimoto tras una serie de tres hechos: mientras él estaba pensando en ideas para un nuevo manga y pensaba que sería ideal que existiera una máquina que le diera ideas, fue entonces cuando tropezó con el juguete de su hija y seguidamente, sintió una pelea de gatos en su vecindario</span>
+</br>
+  ";
+
+echo "<p> Texto dela página .... </p>";
+
+echo "<p> Texto dela página .... </p>";
+
+echo "<p> Texto dela página .... </p>
+         <h1> Holaaaaaa .... </h1>
+     <h2> Texto dela página .... </h2>
+     ";
 
 ?>
+
 
 
 
