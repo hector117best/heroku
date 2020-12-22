31104a8558d7fba50f7acde765c7b5cd8480aefd
hector <hferb10@gmail.com>
2020-12-22 13:45:06 +0100

    Update pagina1.php

--- pagina1.php
+++ pagina1.php
@@ -5,22 +5,20 @@
 
 
 
+
+
 <?php
 
-echo "<h1> DORAEMON </h1><br/>"
+echo "<h1> DORAEMON  </h1><br/>"; 
 
-echo "<p> DATOS</p><p> Doraemon es un personaje ficticio protagonista de la serie de manga y anime del mismo nombre, creado por Fujiko Fujio, seudónimo de Hiroshi Fujimoto y Motoo Abiko. Doraemon es un gato robot que viaja en el tiempo desde el siglo XXI para ayudar a un niño preadolescente llamado Nobita Nobi. </p>";
+echo "<p>  DATOS</p><p> Doraemon es un personaje ficticio protagonista de la serie de manga y anime del mismo nombre, creado por Fujiko Fujio, seudónimo de Hiroshi Fujimoto y Motoo Abiko. Doraemon es un gato robot que viaja en el tiempo desde el siglo XXI para ayudar a un niño preadolescente llamado Nobita Nobi.</p>";
 
-echo "<p> CURIOSIDADES</p>
-<span> Doraemon fue concebido originalmente por Hiroshi Fujimoto tras una serie de tres hechos: mientras él estaba pensando en ideas para un nuevo manga y pensaba que sería ideal que existiera una máquina que le diera ideas, fue entonces cuando tropezó con el juguete de su hija y seguidamente, sintió una pelea de gatos en su vecindario</span>";
+echo "<p>  CURIOSIDADES</p>";
 
-echo "<p><img   src="image/descargar (1).jpg" width= "1300" height="500" align="center"/></p>";
+echo "<p>Doraemon fue concebido originalmente por Hiroshi Fujimoto tras una serie de tres hechos: mientras él estaba pensando en ideas para un nuevo manga y pensaba que sería ideal que existiera una máquina que le diera ideas, fue entonces cuando tropezó con el juguete de su hija y seguidamente, sintió una pelea de gatos en su vecindario</p>";
 
-
- 
-
-
-
-
+echo "<p> <img   src="image/descargar (1).jpg" width= "1300" height="500" align="center"/></p>
+    
+     ";
 
 ?>
