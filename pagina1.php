4777f646f74ef51dd76b5fc2c9db4689fc91053c
hector <hferb10@gmail.com>
2020-12-22 12:01:43 +0100

    Update pagina1.php

--- pagina1.php
+++ pagina1.php
@@ -1,7 +1,6 @@
 
 
-<!DOCTYPE html> 
-<html lang="es">
+
 
 
 
@@ -16,13 +15,87 @@ echo "<p> Texto dela página .... </p>";
 
 echo "<p> Texto dela página .... </p>";
 
-echo "<p> Texto dela página .... </p>
-         <h1> Holaaaaaa .... </h1>
-     <h2> Texto dela página .... </h2>
-     ";
+echo "<head>
+        <title>Este texto es el título del documento</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="estilos.css" type="text/css">
+<link href="css/estilos.css" rel="stylesheet" type="text/css" >
+        <style>
+ 
+                .dv { background-image: url("rombos.jpg"); }
+        </style>
+</head>
+<body style="background-color:#EC7063;">
+        <header>
+                <div style="background-image: url('rombos.jpg'); border: 3px solid #186A3B ; "> <h1> GASTRONOMÍA NAVIDEÑA </h1>  </div>
+                <div class="dv"> Felices Fiestas</div>
+
+        </header>
+
+
+
+<img src="navidad1.jpg" width="700" height="700" align="right"/>
+
+
+        <main>
+                <a href="pagina1.php"> RECETAS </a><br/>
+</br>
+</br>
+                <a href="pagina2.php"> POSTRES </a>
+
+        </main>
+        <section>
+</br>
+               </br>
+                        <p class="mitexto1" align="left"> “La navidad no es una fecha; es un estado en la mente” (Mary Ellen Chase) </p>
+</br>
+</br>
+                        <p class="mitexto2"> “No existe la Navidad ideal, solo la Navidad que usted decida crear como reflejo de sus valores, deseos, queridos y tradiciones” (Bill McKibben)</p>
+                       </br>
+  </br>
+ 
+                        <center>
+
+                        <h1> PIRULETAS DE NAVIDAD </h1>
+                        <p> INGREDIENTES</p>
+                        <span> Surtido variado de sprinkles con motivos o colores navideños </span>
+</br>
+                        <span> 15 palitos para piruletas </span>
+</br>
+                        <span> 425 g de azúcar </span>
+</br>
+<span> 160 ml de azúcar invertido o sirope de maíz</span>
+</br>
+<span> 125 ml de agua</span>
+</br>
+<span> Una pizca de sal</span>
+</br>
+<span> Una o dos gotitas de aroma concentrado de Amaretto </span>
+</br>
+<p> Modo de preparación</p>
+<span> Ponemos los sprinkles y el acebo de fondant en varios cuencos pequeños o ramequines de tal forma que estén separados por colores y formas. </span>
+</br>
+<span> En un vaso ponemos los palitos, a un lado de los cuencos con sprinkles. Necesitaremos trabajar con ellos rápidamente antes de que se endurezcan las piruletas.</span>
+                       
+  </center>
+                        <br/><br/>
+                        <center>
+                       
+                       
+                       
+                       
+                       
+                        </center>
+                       
+        </section>
+                <br/>
+               
+<div style="background-image: url('rombos.jpg'); border: 3px solid #186A3B ; "> <h1> ¡A comer!</h1>  </div>
+                       
+
+</body>;
 
 ?>
 
 
 
-</html>
